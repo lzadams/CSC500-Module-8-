a946709260924a9cb689f610340d6d4242d38604
lzadams <lili.adams@outlook.com>
2024-08-02 16:05:22 -0700

    Update Module 8.py

--- Module 8.py
+++ Module 8.py
@@ -11,23 +11,23 @@ class ItemToPurchase:
         return total_cost
 
 # #Step 2: In the main section of your code, prompt the user for two items and create two objects of the ItemToPurchase class.
-# items = []
-# for i in range(2):
-#     print(f'Item {i + 1}')
-#     item = ItemToPurchase()
-#     item.name = input('Enter the item name:\n')
-#     item.price = float(input('Enter the item price:\n'))
-#     item.quantity = int(input('Enter the item quantity:\n'))
-#     items.append(item)
-#     item_cost = item.print_item_cost()
-#     i = i + 1
-#
-# #Step 3: Add the costs of the two items together and output the total cost.
-# print('TOTAL COST')
-# total_cost = 0
-# for item in items:
-#     total_cost += item.print_item_cost()
-# print(f'Total: ${total_cost}')
+items = []
+for i in range(2):
+    print(f'Item {i + 1}')
+    item = ItemToPurchase()
+    item.name = input('Enter the item name:\n')
+    item.price = float(input('Enter the item price:\n'))
+    item.quantity = int(input('Enter the item quantity:\n'))
+    items.append(item)
+    item_cost = item.print_item_cost()
+    i = i + 1
+
+#Step 3: Add the costs of the two items together and output the total cost.
+print('TOTAL COST')
+total_cost = 0
+for item in items:
+    total_cost += item.print_item_cost()
+print(f'Total: ${total_cost}')
 
 #Step 4:Build the ShoppingCart class with the following data attributes and related methods.
 # Note: Some can be method stubs (empty methods) initially, to be completed in later steps
